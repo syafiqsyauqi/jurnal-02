50bfd8a131a8639b12a1aad0ffa11bc98f2e04b9
syafiqsyauqi <91374062+syafiqsyauqi@users.noreply.github.com>
2021-09-30 18:32:59 +0700

    baris baru

--- Tugas.java
+++ Tugas.java
@@ -2,8 +2,8 @@ public class Tugas {
     
 	public static void main(String[] agrs) {
 	    System.out.println("NAMA     :MUHAMMAD SYAFIQ SYAUQI RAMADHAN");
-        System.out.println("NIM      :6706210157");
-        System.out.println("KELAS    :D3RPLA-45-05");
-		System.out.println("IPK      :4,0");
+            System.out.println("NIM      :6706210157");
+            System.out.println("KELAS    :D3RPLA-45-05");
+	    System.out.println("IPK      :4,0");
     }
 }	
