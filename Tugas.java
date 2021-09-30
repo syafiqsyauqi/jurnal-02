fcb84577014ebf601800ffac1bab7751be3595be
syafiqsyauqi <syafiqsayauqi476@gmail.com>
2021-09-30 18:31:14 +0700

    tugas jurnal

---
+++ Tugas.java
@@ -0,0 +1,9 @@
+public class Tugas {
+    
+	public static void main(String[] agrs) {
+	    System.out.println("NAMA     :MUHAMMAD SYAFIQ SYAUQI RAMADHAN");
+        System.out.println("NIM      :6706210157");
+        System.out.println("KELAS    :D3RPLA-45-05");
+		System.out.println("IPK      :4,0");
+    }
+}	
