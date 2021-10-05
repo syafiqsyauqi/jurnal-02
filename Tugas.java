2c0b1c1aca48a4dd3ac8b135a05ff56b2b7a2e50
syafiqsyauqi <91374062+syafiqsyauqi@users.noreply.github.com>
2021-10-05 10:36:32 +0700

    betulin script

--- Tugas.java
+++ Tugas.java
@@ -1,9 +1,14 @@
 public class Tugas {
     
 	public static void main(String[] agrs) {
-	    System.out.println("NAMA     :MUHAMMAD SYAFIQ SYAUQI RAMADHAN");
-            System.out.println("NIM      :6706210157");
-            System.out.println("KELAS    :D3RPLA-45-05");
-	    System.out.println("IPK      :4,0");
+	     double nilai = 4.00;
+	    String nilai2 = "IPK  :";
+	    String nama = "Muhammad syafiq syauqi ramadhan";
+	    String nim = "6706210157";
+	    String kelas = " D3RPLA4505";
+	    System.out.println("NAMA     :" + "MUHAMMAD SYAFIQ SYAUQI RAMADHAN");
+            System.out.println("NIM      :" + "6706210157");
+            System.out.println("KELAS    :" + "D3RPLA-45-05");
+	    System.out.println("IPK      :" + "4,0");
     }
 }	
